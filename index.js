9c2d6ae56c52acab55c589ed4f82b6dd994eab86
garthdb <garthdb@gmail.com>
2014-12-26 15:40:49 -0700

    successfully able to add points and view realtime updates

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ var stylus = require("stylus");
 var app = express();
 var pub = __dirname + '/public';
 var port = process.env.PORT || 5000;
-var userid = process.env.userid || 'admin';
-var passid = process.env.password || '12345';
+var auth = require('http-auth');
+var userid = process.env.userid || 'admin'; //TODO Setup basic auth
+var pass = process.env.password || '12345';
+
+var basic = auth.basic({
+    realm: "Simon Area."
+  }, function (username, password, callback) { // Custom authentication method.
+    callback(username === userid && password === pass);
+  }
+);
+
+var authMiddleware = auth.connect(basic);
 
 app.use(stylus.middleware(
   { src: __dirname + '/public'
@@ -21,41 +31,49 @@ app.use(express.static(pub));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+var houses = [
+{id: 1, name: 'Team 1', points: 1},
+{id: 2, name: 'Team 2', points: 2},
+{id: 3, name: 'Team 3', points: 3},
+{id: 4, name: 'Team 4', points: 4}
+];
+
 app.get('/', function(req, res){
-  res.render('scoreboard', {houses: [
-    {name: 'yo', points: 1},
-    {name: 'yup', points: 2},
-    {name: 'too', points: 3},
-    {name: 'totes', points: 4}
-  ]});
+  res.render('scoreboard', {houses: houses});
 });
 
-app.get('/scorekeeper', function(req, res){
-  res.render('scorekeeper', {houses: [
-    {name: 'yo', points: 1},
-    {name: 'yup', points: 2},
-    {name: 'too', points: 3},
-    {name: 'totes', points: 4}
-  ]});
+app.get('/scorekeeper', authMiddleware, function(req, res){
+  res.render('scorekeeper', {houses: houses});
 });
 
 var server = http.createServer(app)
 server.listen(port)
+
+/* Web Socket Server */
 
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+var clients = [];
+
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
   console.log("websocket connection open")
+  clients.push(ws);
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+  ws.send(JSON.stringify(houses));
+
+  ws.on('message', function(message) {
+    for (var i = 0; i < houses.length; i++) {
+      if(houses[i].name == message) {
+        houses[i].points++;
+        for(var j = 0; j < clients.length; j ++){
+          clients[j].send(JSON.stringify([houses[i]]));
+        }
+        break;
+      }
+    }
+    console.log(houses);
+  });
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,25 @@
 var host = location.origin.replace(/^http/, 'ws');
 var ws = new WebSocket(host);
 ws.onmessage = function (event) {
-  var li = document.createElement('li');
-  li.innerHTML = JSON.parse(event.data);
-  document.querySelector('#pings').appendChild(li);
+  var houses = JSON.parse(event.data);
+  console.log(houses);
+  for (var i = 0; i < houses.length; i++) {
+    var houseSelector = '[data-house-id='+ houses[i].id +']';
+    var houseElem = Sizzle(houseSelector);
+    Sizzle(houseSelector+' .housename')[0].innerHTML = houses[i].name;
+    Sizzle(houseSelector+' .housepoints')[0].innerHTML = houses[i].points;
+  }
+  // var li = document.createElement('li');
+  // li.innerHTML = JSON.parse(event.data);
+  // document.querySelector('#pings').appendChild(li);
+};
+ws.onopen = function (event) {
+  var buttons = Sizzle('button')
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function(e){
+      var house = e.target.dataset.house;
+      ws.send(house);
+    };
+  }
+  console.log('opened');
 };
